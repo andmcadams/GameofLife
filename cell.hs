2e0aa6f5b5d2b8b72126468d3bdc80063db5791f
Andrew McAdams <andmcadams@gmail.com>
2017-04-22 08:50:34 -0500

    Alter isAlive algo to run faster by exiting earlier if possible

--- cell.hs
+++ cell.hs
@@ -15,10 +15,17 @@ nextGen xs delt = [y | y <- [0..(len^2 - 1)], isAlive xs y delt]
   where len = round (2/delt)
 
 isAlive :: [Int] -> Int -> Float -> Bool
-isAlive xs cell delt = if elem cell xs then (alive - 1) == 3 || (alive - 1) == 2 else alive == 3
-  where top = break (> (cell - len + 1)) $ snd (break (>= (cell - len - 1)) xs)
-        mid = break (> (cell + 1)) $ snd (break (>= (cell - 1)) $ snd top)
-        bot = break (> (cell + len + 1)) $ snd (break (>= (cell + len - 1)) $ snd mid)
-        alive = (length $ [k | k <- (fst top), m == ((quot k len) + 1)]) + (length $ [k | k <- (fst mid), m == (quot k len)]) + (length $ [k | k <- (fst bot), m == ((quot k len) - 1)])
-        len = round (2/delt)
-        m = quot cell len
+isAlive xs cell delt
+                    | (toplen + midlen) > 3 = False
+                    | alive == 3 = True
+                    | alive == 2 = elem cell xs
+                    | otherwise = False
+                      where top = break (> (cell - len + 1)) $ snd (break (>= (cell - len - 1)) xs)
+                            mid = break (> (cell + 1)) $ snd (break (>= (cell - 1)) $ snd top)
+                            bot = break (> (cell + len + 1)) $ snd (break (>= (cell + len - 1)) $ snd mid)
+                            toplen = length [k | k <- (fst top), m == ((quot k len) + 1)]
+                            midlen = length [k | k <- (fst mid), m == (quot k len), k /= cell]
+                            botlen = length [k | k <- (fst bot), m == ((quot k len) - 1)]
+                            alive = toplen + midlen + botlen
+                            len = round (2/delt)
+                            m = quot cell len
