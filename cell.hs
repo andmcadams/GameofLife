71c94ce0ba176fc202fd9ec13def3201fa63dc5c
Andrew McAdams <andmcadams@gmail.com>
2017-04-22 09:32:35 -0500

    Add parallelism to nextGen computation

--- cell.hs
+++ cell.hs
@@ -1,5 +1,7 @@
 module Cell where
- 
+
+import Control.Parallel
+
 --GLUT
 import Graphics.UI.GLUT
  
@@ -11,8 +13,10 @@ square x y delt = renderPrimitive Quads $ mapM_ vertex3f
   [ ( x, y, 0), (x, (y - delt), 0), ((x + delt), (y - delt), 0), ((x + delt), y, 0) ]
 
 nextGen :: [Int] -> Float -> [Int]
-nextGen xs delt = [y | y <- [0..(len^2 - 1)], isAlive xs y delt]
-  where len = round (2/delt)
+nextGen xs delt = par p1 (pseq p2 (p1 ++ p2)) --[y | y <- [0..(len^2 - 1)], isAlive xs y delt]
+  where len = (round (2/delt))^2 - 1
+        p1 = [y | y <- [0..(quot len 2)], isAlive xs y delt]
+        p2 = [y | y <- [((quot len 2) + 1)..len], isAlive xs y delt]
 
 isAlive :: [Int] -> Int -> Float -> Bool
 isAlive xs cell delt
